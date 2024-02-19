73f00a2d7f70e9da27c1b395b63c82e7e3d0011d
alatiff045@gmail.com <alatiff045@gmail.com>
2024-02-19 11:27:01 +0700

    menambahkan

---
+++ main.js
@@ -0,0 +1,45 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  getDoc,
+  deleteDoc,
+  doc,
+  query,
+  orderBy,
+  updateDoc
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCdgAITXTldockRB_wgxOrbNUPKzSVBhDs",
+  authDomain: "insan-cemerlang-c9554.firebaseapp.com",
+  projectId: "insan-cemerlang-c9554",
+  storageBucket: "insan-cemerlang-c9554.appspot.com",
+  messagingSenderId: "753628555075",
+  appId: "1:753628555075:web:7a72b2d1e8ae89716931f6",
+  measurementId: "G-KMJZ5V0B8H"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export async function ambilDaftarProduk(){
+  const refDokumen = collection(db, "produk");
+  const query = query(refDokumen, orderBy("nama"));
+  const cuplikanKuery = await getDocs(query)
+  
+  let hasil = [];
+  cuplikanKuery.forEach((doc) => {
+    hasil.push({id: doc.id,
+    nama: doc.data().nama,
+    harga:doc.data().harga,
+    stok: dok.data().stok,
+    });
+  });
+  
+  return hasil;
+}
